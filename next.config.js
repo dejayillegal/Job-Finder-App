32c877ffcc8d9f629c7605bbb2b79eea135d078d
dejayillegal <119801523+dejayillegal@users.noreply.github.com>
2025-10-26 21:24:10 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // Required for GitHub Pages
   images: {
-    unoptimized: true,
+    unoptimized: true, // Disable Next.js image optimization for static export
   },
+  basePath: '/Job-Finder-App', // 👈 your repo name here
+  assetPrefix: '/Job-Finder-App/',
 };
 
 module.exports = nextConfig;
